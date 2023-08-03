fbda749b4a9e459401e18f7e26fd57b94915ae79
RA1117 <Ryu1117@softbank.ne.jp>
2023-08-03 14:25:28 +0900

    認証機能2

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -8,32 +8,39 @@
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     </head>
-    <body class="antialisased">
-        <h1>Blog Name</h1>
-        <form action='/posts' method='POST'>
-            @csrf
-            <div class='title'>
-                <h2>Title</h2>
-                <input type='text' name=post[title] placeholder='タイトル' value={{old('post.title')}}>
-                <p class='title_error' style='color:red'>{{ $errors->first('post.title') }}</p>
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create') }}
+        </h2>
+        </x-slot>
+        <body class="antialisased">
+            <h1>Blog Name</h1>
+            <form action='/posts' method='POST'>
+                @csrf
+                <div class='title'>
+                    <h2>Title</h2>
+                    <input type='text' name=post[title] placeholder='タイトル' value={{old('post.title')}}>
+                    <p class='title_error' style='color:red'>{{ $errors->first('post.title') }}</p>
+                </div>
+                <div class='body'>
+                    <h2>Body</h2>
+                    <textarea name='post[body]' placeholder='今日も一日お疲れ様でした。'>{{old('post.body')}}</textarea>
+                    <p class='body_error' style='color:red'>{{ $errors->first('post.body') }}</p>
+                </div>
+                <input type="submit" value='store'>
+                <div class='category'>
+                <h2>Category</h2>
+                <select name='post[category_id]'>
+                    @foreach($categories as $category)
+                        <option value='{{ $category->id}}'>{{ $category->name}}</option>
+                    @endforeach
+                </select>
             </div>
-            <div class='body'>
-                <h2>Body</h2>
-                <textarea name='post[body]' placeholder='今日も一日お疲れ様でした。'>{{old('post.body')}}</textarea>
-                <p class='body_error' style='color:red'>{{ $errors->first('post.body') }}</p>
+            </form>
+            <div class = 'footer'>
+                <a href = '/'>戻る</a>
             </div>
-            <input type="submit" value='store'>
-            <div class='category'>
-            <h2>Category</h2>
-            <select name='post[category_id]'>
-                @foreach($categories as $category)
-                    <option value='{{ $category->id}}'>{{ $category->name}}</option>
-                @endforeach
-            </select>
-        </div>
-        </form>
-        <div class = 'footer'>
-            <a href = '/'>戻る</a>
-        </div>
-    </body>
+        </body>
+    </x-app-layout>
 </html>
--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -8,32 +8,40 @@
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     </head>
-    <body class="antialiased">
-        <h1>Blog Name</h1>
-        <a href='/posts/create'>create</a>
-        <div class='posts'>
-            @foreach($posts as $post)
-                 <div class='post'>
-                    <a href='/posts/{{ $post->id }}'><h2 class='title'>{{ $post->title }}</h2></a>
-                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-                    <p class='body'>{{ $post->body }}</p>
-                    <form action='/posts/{{ $post->id }}' id='form_{{ $post->id }}' method='post'>
-                        @csrf
-                        @method('DELETE')
-                         <button type='button' onclick='deletePost({{ $post->id }})'>delete</button>
-                    </form>
-                </div>
-            @endforeach
-        </div>
-        <div class='paginate'>{{ $posts->links()}}</div>
-        <script>
-            function deletePost(id) {
-                'use strict'
-                
-                if(confirm('削除すると復元できません。\n本当に削除しますか？')){
-                    document.getElementById(`form_${id}`).submit();
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Index') }}
+        </h2>
+        </x-slot>
+        <body class="antialiased">
+            <h1>Blog Name</h1>
+            <a href='/posts/create'>create</a>
+            <div class='posts'>
+                @foreach($posts as $post)
+                     <div class='post'>
+                        <a href='/posts/{{ $post->id }}'><h2 class='title'>{{ $post->title }}</h2></a>
+                        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                        <p class='body'>{{ $post->body }}</p>
+                        <form action='/posts/{{ $post->id }}' id='form_{{ $post->id }}' method='post'>
+                            @csrf
+                            @method('DELETE')
+                             <button type='button' onclick='deletePost({{ $post->id }})'>delete</button>
+                        </form>
+                        <p>ログインユーザー：{{ Auth::user()->name }}</p>
+                    </div>
+                @endforeach
+            </div>
+            <div class='paginate'>{{ $posts->links()}}</div>
+            <script>
+                function deletePost(id) {
+                    'use strict'
+                    
+                    if(confirm('削除すると復元できません。\n本当に削除しますか？')){
+                        document.getElementById(`form_${id}`).submit();
+                    }
                 }
-            }
-        </script>
-    </body>
+            </script>
+        </body>
+    </x-app-layout>
 </html>
